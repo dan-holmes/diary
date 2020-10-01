ddca088e5d02b65c3f9f1f2cccb9147a661ec01e
Dan Holmes <dan@Dans-MacBook-Air.local>
2020-10-01 12:10:40 +0100

    Remove repetition in test set up

--- spec/SecretDiary_spec.rb
+++ spec/SecretDiary_spec.rb
@@ -1,60 +1,50 @@
 require "SecretDiary"
 
 describe SecretDiary do
-  it "can add an entry" do
-    # Arrange
-    diary = SecretDiary.new
+  # Arrange
+  before(:each) do
+    @diary = SecretDiary.new
+  end
 
+  it "can add an entry" do
     # Act
-    diary.add_entry("This is my entry.")
+    @diary.add_entry("This is my entry.")
 
     # Assert
-    expect(diary.entries[0]).to eq "This is my entry."
+    expect(@diary.entries[0]).to eq "This is my entry."
   end
 
   it "can get entries" do
-    # Arrange
-    diary = SecretDiary.new
-
     # Act
-    diary.add_entry("This is my entry.")
+    @diary.add_entry("This is my entry.")
 
     # Assert
-    expect(diary.get_entries).to eq ["This is my entry."]
+    expect(@diary.get_entries).to eq ["This is my entry."]
   end
 
   it "cannot add entries when locked" do
-    # Arrange
-    diary = SecretDiary.new
-
     # Act
-    diary.lock
+    @diary.lock
 
     # Assert
-    expect { diary.add_entry("This is my entry.") }.to raise_error "Please unlock the diary first."
+    expect { @diary.add_entry("This is my entry.") }.to raise_error "Please unlock the diary first."
   end
 
   it "cannot get entries when locked" do
-    # Arrange
-    diary = SecretDiary.new
-
     # Act
-    diary.lock
+    @diary.lock
 
     # Assert
-    expect { diary.get_entries }.to raise_error "Please unlock the diary first."
+    expect { @diary.get_entries }.to raise_error "Please unlock the diary first."
   end
 
   it "doesn't error when locked and unlocked again" do
-    # Arrange
-    diary = SecretDiary.new
-
     # Act
-    diary.lock
-    diary.unlock
+    @diary.lock
+    @diary.unlock
 
     # Arrange
-    expect { diary.add_entry("This is my entry.") }.not_to raise_error
-    expect { diary.get_entries }.not_to raise_error
+    expect { @diary.add_entry("This is my entry.") }.not_to raise_error
+    expect { @diary.get_entries }.not_to raise_error
   end
 end
